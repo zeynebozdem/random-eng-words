b504f5e3eedfc05473ae286eb57c97fa5ef2fe38
Zeynep Özdem Özdem <zeynebozdem@gmail.com>
2020-10-09 19:50:36 +0300

    words added

--- src/components/WordBox/WordBox.js
+++ src/components/WordBox/WordBox.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import styles from './WordBox.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRedoAlt } from '@fortawesome/free-solid-svg-icons'
+
 
 import data from '../../data/words.json'
 
 const wordData = data;
+const randomNumber = Math.floor(Math.random() * wordData.length);
+const randomWord = wordData[randomNumber].word;
 class WordBox extends Component {
     render() { 
         const wordColor = this.props.color;
@@ -11,6 +16,7 @@ class WordBox extends Component {
             <div className={styles.wordBox}>
                 <h1>
                     <a style={{color:wordColor}} href={`https://translate.google.com/?sl=en&tl=ar#view=home&op=translate&sl=en&tl=tr&text=${randomWord}`} target="_blank">{randomWord}</a>
+                    <FontAwesomeIcon icon={faRedoAlt}/>
                 </h1>
             </div>
          );
